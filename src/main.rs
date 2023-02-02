c929fd2063032b166c19e34b22da13f7347e9a26
Emil Lundberg <emil@yubico.com>
2023-02-02 18:32:47 +0100

    Add yew example app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use yew::function_component;
+use yew::html;
+use yew::use_state;
+use yew::Html;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 20;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
